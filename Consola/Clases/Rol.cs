c4a1fe91b7b718e3475c892bae2b452ad3430c30
Fabián Vargas <vargaszen@users.noreply.github.com>
2019-04-07 16:47:49 -0500

    Otro cambio !!!

---
+++ Consola/Clases/Rol.cs
@@ -0,0 +1,12 @@
+namespace Consola
+{    
+    using System;
+
+    [Serializable]
+    public class Rol
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
